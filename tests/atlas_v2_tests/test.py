199763000ba4791b9de733a2304cc5b3911b95b2
u202293 <markus.meier-trost@sbb.ch>
2026-03-05 08:21:11 +0100

    Improved atlas_v2_tests.

--- tests/atlas_v2_tests/test.py
+++ tests/atlas_v2_tests/test.py
@@ -29,6 +29,7 @@ def run() -> dict:
     data_test = DataTest(name="atlas_v2_test")
     ref_sizes = load_json_file(REF_SIZES_FILE)
     sizes = defaultdict(lambda : {})
+    sucesses = ""
     for dataset in DATASETS:
         meta_data, size, data_test = load_ckan_package(f"{dataset}-v2", data_test)
         for i, flavour in enumerate(FLAVOURS):
@@ -36,26 +37,31 @@ def run() -> dict:
                 region = "swiss-" if dataset == "service-point" else ("world-" if dataset == "traffic-point" else "")
                 identifier = f"{flavour}-{region}{dataset}.csv"
                 url = f"https://data.opentransportdata.swiss/dataset/{dataset}-v2/resource_permalink/{identifier}"
-                header, rows, status_code, data_test = load_csv_from_url(url, data_test=data_test)
+                header, rows, status_code, data_test = load_csv_from_url(url, data_test=data_test, silent=True)
                 if status_code < 400:
                     if ref_sizes:
                         ref_sizes_ds = ref_sizes.get(dataset)
                         if ref_sizes_ds:
                             ref_size = ref_sizes_ds.get(flavour)
                             if ref_size is not None and type(ref_size) is int:
-                                data_test.test(SIZE_THRESHOLDS[0] * ref_size < size < SIZE_THRESHOLDS[1] * ref_size,
-                                               # silent if ok:  if_true_log_info=f"Passed size check, has {len(rows)} rows.",
-                                               if_false_log_warning=f"Resource size {size} is not within {SIZE_THRESHOLDS} of reference size {ref_size}.")
-                                size = round(ALPHA * size + (1 - ALPHA) * ref_size) # Exponential Moving Average (EMA)
+                                if not (SIZE_THRESHOLDS[0] * ref_size < size < SIZE_THRESHOLDS[1] * ref_size):
+                                    data_test.log_warning(f"Resource size {size} is not within {SIZE_THRESHOLDS} of reference size {ref_size}.")
+                                    size = round(ALPHA * size + (1 - ALPHA) * ref_size) # Exponential Moving Average (EMA)
                     sizes[dataset][flavour] = size
 
                     metadata_resource = resource_by_identifier(meta_data, identifier)
                     age = age_in_days(metadata_resource.get("issued"))
-                    data_test.test(age < AGE_IN_DAYS_THRESHOLD,
-                                   # silent if ok: if_true_log_info=f"Passed age check, age {age:.4f} is below threshold {AGE_IN_DAYS_THRESHOLD:.4f}.",
-                                   if_false_log_failure=f"FAILED age check, age {age:.4f} is above threshold {AGE_IN_DAYS_THRESHOLD:.4f}.")
+                    if age >= AGE_IN_DAYS_THRESHOLD:
+                        data_test.log_failure(f"FAILED age check, age {age:.4f} is above threshold {AGE_IN_DAYS_THRESHOLD:.4f}.")
+                    else:
+                        sucesses = sucesses + identifier + ', '
+                else:
+                    data_test.log_failure(f"Response {status_code} for {identifier}.")
+
             except Exception as e:
                 data_test.log_exception(f"Data test for {dataset}/{flavour} failed with exception: {e}", e)
+
+    data_test.log_info(f"Sucessfully tested: {sucesses[:-2]}.")
 
     save_json_file(REF_SIZES_FILE, sizes)
 
@@ -65,4 +71,3 @@ def run() -> dict:
 if __name__ == '__main__':
     tr = run()
     print(tr)
-    print(tr['logs'])
