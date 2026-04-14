9f4d22b4df8c05b6c9154bcc961ae90d497d512c
u202293 <markus.meier-trost@sbb.ch>
2026-04-14 14:00:26 +0200

    ojp20...tests: Bug fixes 3.

--- tests/ojp20_tr_random_validated_tests/test.py
+++ tests/ojp20_tr_random_validated_tests/test.py
@@ -48,7 +48,7 @@ def _dump_to_file(object, **kwargs):
     for k, v in kwargs.items():
         s += f"_{str(k)}_{str(v)}"
     with open(get_path(DUMP_FILE.replace('$$', s)), "w", encoding='utf-8-sig') as f:
-        f.write(json.dumps(object, indent=4))
+        f.write(object)
 
 
 def run():
