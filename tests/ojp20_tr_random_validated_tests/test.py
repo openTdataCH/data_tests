6a3b0b034910958ba812e13270a4b78cd8cefd91
u202293 <markus.meier-trost@sbb.ch>
2026-04-14 14:14:33 +0200

    ojp20...tests: Bug fixes 4.

--- tests/ojp20_tr_random_validated_tests/test.py
+++ tests/ojp20_tr_random_validated_tests/test.py
@@ -65,38 +65,41 @@ def run():
     with open(get_path(CONN_LOG_FILE), mode='a', encoding='utf-8') as conn_log:
         conn_log.write(f"{'_'*200}\n{_timestamp()} Running data_test '{TEST_NAME}', {number_of_tests} random TR connections:\n")
         for i in range(0, number_of_tests):
-            time.sleep(config['sleep_time'])
-            origin_ref = random.choice(stops_ids)
-            while True:
-                destin_ref = random.choice(stops_ids)
-                if destin_ref != origin_ref:
-                    break
-            t0 = time.time()
-            status, size, resp_dict = ojp20_triprequest(origin_ref, destin_ref, return_as='dict', data_test=data_test)
-            delta_t = time.time() - t0
-            t += delta_t
-            conn_text = f"{origin_ref} {stops[origin_ref]:30}-> {destin_ref} {stops[destin_ref]:30}"
-            resp_excp = str(resp_dict)[:30]
+            try:
+                time.sleep(config['sleep_time'])
+                origin_ref = random.choice(stops_ids)
+                while True:
+                    destin_ref = random.choice(stops_ids)
+                    if destin_ref != origin_ref:
+                        break
+                t0 = time.time()
+                status, size, resp_dict = ojp20_triprequest(origin_ref, destin_ref, return_as='dict', data_test=data_test)
+                delta_t = time.time() - t0
+                t += delta_t
+                conn_text = f"{origin_ref} {str(stops[origin_ref]):30}-> {destin_ref} {str(stops[destin_ref]):30}"
+                resp_excp = str(resp_dict)[:30]
 
-            conn_log.write(f'{_timestamp()} {conn_text}: {delta_t:.3f} sec.,{size:>9} bytes, status={status}, excerpt={resp_excp}...\n')
+                conn_log.write(f'{_timestamp()} {conn_text}: {delta_t:.3f} sec.,{size:>9} bytes, status={status}, excerpt={resp_excp}...\n')
 
-            if status != 200 or resp_dict.get('ERROR') is not None:
-                data_test.log_failure(f"OJP2.0 TR with {conn_text} failed with status code {status}, excerpt: {resp_excp}...")
-            else:
-                count200 += 1
-                jsondata = ""
-                try:
-                    jsondata = json.dumps(resp_dict, ensure_ascii=False, indent=2)
-                    validate(instance=resp_dict, schema=json_schema)
-                except ValidationError as e:
-                    data_test.log_warning(f"Validation error: {str(e.message)}")
-                    _dump_to_file(jsondata, status=status, case="valerr")
-                    count_invalid += 1
-                except SchemaError as e:
-                    data_test.log_warning(f"Invalid schema: {str(e)}", e)
-                except Exception as e:
-                    data_test.log_warning(f"OJP2.0 TR with {conn_text} processing failed with {str(e)}, excerpt: {resp_excp}...")
-                    _dump_to_file(jsondata, status=status, case="exc")
+                if status != 200 or resp_dict.get('ERROR') is not None:
+                    data_test.log_failure(f"OJP2.0 TR with {conn_text} failed with status code {status}, excerpt: {resp_excp}...")
+                else:
+                    count200 += 1
+                    jsondata = ""
+                    try:
+                        jsondata = json.dumps(resp_dict, ensure_ascii=False, indent=2)
+                        validate(instance=resp_dict, schema=json_schema)
+                    except ValidationError as e:
+                        data_test.log_warning(f"Validation error: {str(e.message)}")
+                        _dump_to_file(jsondata, status=status, case="valerr")
+                        count_invalid += 1
+                    except SchemaError as e:
+                        data_test.log_warning(f"Invalid schema: {str(e)}", e)
+                    except Exception as e:
+                        data_test.log_warning(f"OJP2.0 TR with {conn_text} processing failed with {str(e)}, excerpt: {resp_excp}...")
+                        _dump_to_file(jsondata, status=status, case="exc")
+            except Exception as e:
+                data_test.log_exception(f'Test {i} failed with Exception {str(e)}', e)
 
         data_test.log_info(f"Performed {number_of_tests} OJP2.0 TR: {count200} ok (status 200), {count_valid} valid, {count_invalid} invalid, average {t/number_of_tests:0.3f} seconds.")
         if t / number_of_tests > warning_threshold_sec_per_test:
