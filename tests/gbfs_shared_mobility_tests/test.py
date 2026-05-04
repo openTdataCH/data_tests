09a270b32ccef361e1e76dc063b4fd1168961556
u227013 <eveline_heidy.walker@sbb.ch>
2026-05-04 09:17:09 +0200

    remove five gbfs sites

--- tests/gbfs_shared_mobility_tests/test.py
+++ tests/gbfs_shared_mobility_tests/test.py
@@ -25,7 +25,12 @@ EXCLUDED_URLS = {
     "https://sharedmobility.ch/v2/gbfs/bird-zurich/system_pricing_plans",
     "https://sharedmobility.ch/v2/gbfs/bird-basel/system_pricing_plans",
     "https://sharedmobility.ch/v2/gbfs/bird-schaffhausen/system_pricing_plans",
-    "https://sharedmobility.ch/v2/gbfs/bird-winterthur/system_pricing_plans"
+    "https://sharedmobility.ch/v2/gbfs/bird-winterthur/system_pricing_plans",
+    "https://sharedmobility.ch/v2/gbfs/voiscooters.com/geofencing_zones",
+    "https://sharedmobility.ch/v2/gbfs/bird-basel/geofencing_zones",
+    "https://sharedmobility.ch/free_bike_status.json",
+    "https://sharedmobility.ch/station_information.json",
+    "https://sharedmobility.ch/v2/gbfs/bolt_zurich/geofencing_zones"
 }
 
 TEST_CONFIGURATIONS_GBFS21 = [
