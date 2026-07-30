760608d64843d11e5c9edab794d9c4d632da4ff7
u202293 <markus.meier-trost@sbb.ch>
2026-07-30 13:02:00 +0200

    TFS test fix for new operatorref prefix "ojp:..." #2

--- tests/train_formation_service_tests/test.py
+++ tests/train_formation_service_tests/test.py
@@ -77,7 +77,8 @@ def get_ojp20_tr_first_tfs_enabled_train_leg(origin_ref: str, destin_ref: str, d
                     if service and service['Mode'].get('PtMode') == 'rail':
                         od = service.get('OperatingDayRef')
                         tn = service.get('TrainNumber')
-                        op = str(service.get('siri:OperatorRef')).replace("ojp:", "")  # strip new prefix as of July 2026
+                        op = service.get('siri:OperatorRef')
+                        op = str(op).replace("ojp:", "") if op is not None else None  # strip new prefix as of July 2026
                         if od and op in TFS_ENABLED_OPERATORS.keys() and tn:
                             return od, op, tn
     except Exception as e:
